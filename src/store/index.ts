839d0603752cb4f287c86d8d0794fcd3e647ebd4
nico-bellante <nicobell@microsoft.com>
2019-09-25 17:27:08 -0700

    adding readme and removing unneeded deps

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer, { IState } from './reducer'
+import { createStore } from 'redux'
+import rootReducer from './reducer'
 
 import * as actions from './actions'
 import * as selectors from './selectors'
 
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { ActionType } from 'typesafe-actions'
-import createSagaMiddleware from '@redux-saga/core'
 
 export const configureStore = () => {
-  const store = createStore(
-    rootReducer,
-    {},
-    composeWithDevTools(applyMiddleware(createSagaMiddleware())),
-  )
+  const store = createStore(rootReducer, {}, composeWithDevTools())
   return store
 }
 
--- src/store/sagas.ts
+++
@@ -1,3 +0,0 @@
-import { all, select } from 'redux-saga/effects'
-
-export default function* rootSaga() {}
